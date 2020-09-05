826f6a10528bc8b0f533466e7463148586079b58
Damaris Göbel <damaris.goebel@gmx.de>
2020-09-05 13:29:11 +0200

    Updates

---
+++ precache-manifest.7e51d94a0a767548b450f4ea387c16ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca6c43f0133e38a43d52d5226ecfb44a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9f8a5f8eb929d058ab02",
+    "url": "/monsters-rolodex/static/css/main.0ae4b0a5.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f8a5f8eb929d058ab02",
+    "url": "/monsters-rolodex/static/js/main.2998f106.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
